5f28bae90847c299a409d68ff6ef884c32413ffc
RenatoMAP77 <renatomapbusiness@gmail.com>
2024-08-02 12:12:20 -0300

    hook funcionando

--- src/app/Hooks/UsePost.ts
+++ src/app/Hooks/UsePost.ts
@@ -2,26 +2,36 @@ import axios, { AxiosResponse } from "axios";
 import { PostData } from "../Interface/PostData";
 import { useQuery } from "@tanstack/react-query";
 
-const API_URL = "https://localhost:8080";
+const API_URL = "http://localhost:8080";
 
-const fetchData = async (): Promise<AxiosResponse<PostData[]>> => {
-    try{
-    const response = await axios.get<PostData[]>(API_URL + "/post");
-    return response;
-} catch (error) {
-    console.error(error);
-    throw new Error("Error fetching data");
-}
-}
+const fetchData = async (): Promise<PostData[]> => {
+    try {
+        const response = await fetch(`${API_URL}/post`);
+        
+        if (!response.ok) {
+            // Se a resposta não for OK, lança um erro
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const data: PostData[] = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw new Error("Error fetching data");
+    }
+};
+
+
+
 export function UsePostData() {
     const query = useQuery({
         queryKey: ['post-data'],
         queryFn: fetchData,
-        retry: 2
+        retry: 2,
     });
 
     return {
         ...query,
-        data: query.data?.data
-    }
-    }
+        data: query.data,
+    };
+}
